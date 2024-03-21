209591b6c663ee443c30e466163d1f7b8c5a584e
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-21 11:16:19 +0100

    eh

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -27,6 +27,25 @@ func _ready():
 	if not OS.has_feature("dedicated_server") and get_tree().get_multiplayer().is_server():
 		player_join(1)
 
+	var terrain = Terrain3D.new()
+	terrain.set_collision_enabled(false)
+	terrain.storage = Terrain3DStorage.new()
+	terrain.texture_list = Terrain3DTextureList.new()
+	add_child(terrain, true)
+	terrain.material.world_background = Terrain3DMaterial.NOISE
+	terrain.texture.name = "Grass"
+	terrain.texture.texture_id = 0
+	var image = load("res://Textures/leafy_grass_diff_4k.jpg")
+	terrain.texture.albedo_texture = image
+	terrain.name = "Terrain3D"
+
+	var noise = FastNoiseLite.new()
+	noise.frequency = 0.0005
+	var img = Image.create(2048, 2048, false, Image.FORMAT_RF)
+	for x in 2048:
+		for y in 2048:
+			img.set_pixel(x,y, Color(noise.get_noise_2d(x,y) * 0.5, 0., 0., 1.))
+	terrain.storage.import_images([img,null,null],  Vector3(0,0,0), 0.0, 300.)
 func _process(_delta):
 
 	match current_weather:
