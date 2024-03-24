a1730eee7d382fd9539b3d5f0a527dd76960ca65
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-24 10:49:17 +0100

    xd

--- Scenes/Earthquake.gd
+++ Scenes/Earthquake.gd
@@ -4,11 +4,10 @@ var shake_strength = 1
 
 func shake_objects(node):
     for child in node.get_children():
-        if child.is_class("Spatial"): # Verifica si el nodo es un Spatial (objeto 3D)
+        if child.is_in_group("player") or child.is_in_group("movable_objects"): # Verifica si el nodo es un Spatial (objeto 3D)
             var x = randi_range(-shake_strength, shake_strength)
-            var y = randi_range(-shake_strength, shake_strength)
             var z = randi_range(-shake_strength, shake_strength)
-            child.position += Vector3(x, y, z)
+            child.position += Vector3(x, 0, z)
 
         # Llama recursivamente a la función para procesar los hijos del nodo actual
         if child.get_child_count() > 0:
