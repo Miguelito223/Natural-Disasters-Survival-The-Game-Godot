762bcd664afc8fff43b277b0e2e286a5ceb1a68e
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-24 20:06:04 +0100

    xd

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -276,6 +276,9 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 
+		if Input.is_action_just_pressed("ui_accept") and IsInWater:
+			velocity.y += JUMP_VELOCITY
+
 		if Input.is_action_pressed("Spring"):
 			SPEED = SPEED_RUN
 		else:
