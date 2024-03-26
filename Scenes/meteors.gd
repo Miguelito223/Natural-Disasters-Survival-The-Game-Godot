d186fdfbb912803baf25541629b41397d16b0057
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-26 17:22:43 +0100

    fixing

--- Scenes/meteors.gd
+++ Scenes/meteors.gd
@@ -1,13 +1,14 @@
 extends RigidBody3D
 
 var explosion_scene = preload("res://Scenes/explosion.tscn")
-
+var body_scale = Vector3(randi_range(1,50),randi_range(1,50),randi_range(1,50))
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.scale = Vector3(randi_range(1,50),randi_range(1,50),randi_range(1,50))
-	self.mass = 1
+	$CollisionShape3D.scale = body_scale
+	$CSGSphere3D.scale = body_scale
+	$Fire.scale = body_scale
+	self.mass = 3 / 4 * PI * $CSGSphere3D.radius
 	self.gravity_scale = Globals.gravity
-	$CollisionShape3D.scale = self.scale
 
 func _on_body_entered(_body):
 	if _body == self:
