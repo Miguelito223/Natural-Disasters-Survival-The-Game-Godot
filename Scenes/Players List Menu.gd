7a30a6c1b22954d394b485724b05474ef84d794a
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-30 22:35:35 +0100

    fixed

--- Scenes/Players List Menu.gd
+++ Scenes/Players List Menu.gd
@@ -2,14 +2,12 @@ extends CanvasLayer
 
 func _ready():
     if Globals.is_networking:
-        self.visible = is_multiplayer_authority()
-
         if not is_multiplayer_authority():
+            self.visible = is_multiplayer_authority()
             return
 
-        self.visible = false
-    else:
-        self.visible = false
+
+    self.visible = false
 
 func _process(_delta):
     if Globals.is_networking:
@@ -18,15 +16,14 @@ func _process(_delta):
             return
 
         # Eliminar todos los hijos del VBoxContainer
-        for child in $Panel/List.get_children():
-            $Panel/List.remove_child(child)
+        for child in $List.get_children():
+            $List.remove_child(child)
 
         # Iterar sobre los jugadores conectados y agregarlos a la lista
         for player_data in Globals.players_conected_array:
             var label = Label.new()
             label.text = player_data.username + " points: " + str(player_data.points)
-            label.set("theme_override_font_sizes/font_size", 3)
-            $Panel/List.add_child(label, true)
+            $List.add_child(label, true)
 
         # Mostrar u ocultar la lista de jugadores según la acción del teclado
         if Input.is_action_just_pressed("List of players"):
