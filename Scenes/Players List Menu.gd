1b16e2e7968c0c842bd01b21d31ac6e27e50dff3
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-31 17:10:46 +0200

    arreglado :D

--- Scenes/Players List Menu.gd
+++ Scenes/Players List Menu.gd
@@ -1,30 +1,30 @@
 extends CanvasLayer
 
 func _ready():
-    if Globals.is_networking:
-        if not is_multiplayer_authority():
-            self.visible = is_multiplayer_authority()
-            return
+	if Globals.is_networking:
+		if not is_multiplayer_authority():
+			self.visible = is_multiplayer_authority()
+			return
 
 
-    self.visible = false
+	self.visible = false
 
 func _process(_delta):
-    if Globals.is_networking:
-        
-        if not is_multiplayer_authority():
-            return
+	if Globals.is_networking:
+		
+		if not is_multiplayer_authority():
+			return
 
-        # Eliminar todos los hijos del VBoxContainer
-        for child in $List.get_children():
-            $List.remove_child(child)
+		# Eliminar todos los hijos del VBoxContainer
+		for child in $List.get_children():
+			$List.remove_child(child)
 
-        # Iterar sobre los jugadores conectados y agregarlos a la lista
-        for player_data in Globals.players_conected_array:
-            var label = Label.new()
-            label.text = player_data.username + " points: " + str(player_data.points)
-            $List.add_child(label, true)
+		# Iterar sobre los jugadores conectados y agregarlos a la lista
+		for player_data in Globals.players_conected_array:
+			var label = Label.new()
+			label.text = player_data.username + " points: " + str(player_data.points)
+			$List.add_child(label, true)
 
-        # Mostrar u ocultar la lista de jugadores según la acción del teclado
-        if Input.is_action_just_pressed("List of players"):
-            self.visible = !self.visible
+		# Mostrar u ocultar la lista de jugadores según la acción del teclado
+		if Input.is_action_just_pressed("List of players"):
+			self.visible = !self.visible
--- Scenes/map.gd
+++ Scenes/map.gd
@@ -20,6 +20,8 @@ const HTerrainTextureSet = preload("res://addons/zylann.hterrain/hterrain_textur
 var noise_seed
 var noise_multiplier = 50.0
 
+var terrain_data_2
+
 # You may want to change paths to your own textures
 var grass_texture = preload("res://Textures/texture-grass-field.jpg")
 
@@ -73,67 +75,77 @@ func _ready():
 func generate_seed():
 	if not Globals.is_networking:
 		noise_seed = randi()
-		receive_seeds(noise_seed)
+		receive_seeds(noise_seed, terrain_data_2)
 	else:
 		if get_tree().get_multiplayer().is_server():
 			noise_seed = randi()
 
 @rpc("any_peer", "call_local")
-func receive_seeds(received_noise_seed):
+func receive_seeds(received_noise_seed, data):
 	print("Recibiendo semillas del jugador")
 	noise_seed = received_noise_seed
 	noise.seed = noise_seed
-	generate_terrain()
+	generate_terrain(data)
 
-func generate_terrain():
+
+@rpc("any_peer", "call_local")
+func generate_terrain(data=null):
 	print("Generating terrain for player")
-	var terrain_data = HTerrainData.new()
-	terrain_data.resize(4097)
+	if data == null:
+		var terrain_data = HTerrainData.new()
+		terrain_data.resize(4097)
 
-	var heightmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
-	var normalmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
-	var splatmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
+		var heightmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
+		var normalmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
+		var splatmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
 
+		
+		for z in range(heightmap.get_height()):
+			for x in range(heightmap.get_width()):
+				# Generate height
+				var h = noise_multiplier * noise.get_noise_2d(x, z)
+
+				# Getting normal by generating extra heights directly from noise,
+				# so map borders won't have seams in case you stitch them
+				var h_right = noise_multiplier * noise.get_noise_2d(x + 0.1, z)
+				var h_forward = noise_multiplier * noise.get_noise_2d(x, z + 0.1)
+				var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
+
+				# Generate texture amounts
+				var splat = splatmap.get_pixel(x, z)
+
+				heightmap.set_pixel(x, z, Color(h, 0, 0))
+				normalmap.set_pixel(x, z, HTerrainData.encode_normal(normal))
+				splatmap.set_pixel(x, z, splat)
+
+		# Commit modifications so they get uploaded to the graphics card
+		var modified_region = Rect2(Vector2(), heightmap.get_size())
+		terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_HEIGHT)
+		terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_NORMAL)
+		terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_SPLAT)
+
+		# Create texture set
+		# NOTE: usually this is not made from script, it can be built with editor tools
+		var texture_set = HTerrainTextureSet.new()
+		texture_set.set_mode(HTerrainTextureSet.MODE_TEXTURES)
+		texture_set.insert_slot(-1)
+		texture_set.set_texture(0, HTerrainTextureSet.TYPE_ALBEDO_BUMP, grass_texture)
+		# Create terrain node
+		var terrain = HTerrain.new()
+		terrain.set_shader_type(HTerrain.SHADER_CLASSIC4_LITE)
+		terrain.set_data(terrain_data)
+		terrain.set_texture_set(texture_set)
+		add_child(terrain, true)
+
+		# No need to call this, but you may need to if you edit the terrain later on
+		terrain.update_collider()
+
+		#set data
+		data = terrain
+		terrain_data_2 = terrain
+	else:
+		add_child(data, true)
 	
-	for z in range(heightmap.get_height()):
-		for x in range(heightmap.get_width()):
-			# Generate height
-			var h = noise_multiplier * noise.get_noise_2d(x, z)
-
-			# Getting normal by generating extra heights directly from noise,
-			# so map borders won't have seams in case you stitch them
-			var h_right = noise_multiplier * noise.get_noise_2d(x + 0.1, z)
-			var h_forward = noise_multiplier * noise.get_noise_2d(x, z + 0.1)
-			var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
-
-			# Generate texture amounts
-			var splat = splatmap.get_pixel(x, z)
-
-			heightmap.set_pixel(x, z, Color(h, 0, 0))
-			normalmap.set_pixel(x, z, HTerrainData.encode_normal(normal))
-			splatmap.set_pixel(x, z, splat)
-
-	# Commit modifications so they get uploaded to the graphics card
-	var modified_region = Rect2(Vector2(), heightmap.get_size())
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_HEIGHT)
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_NORMAL)
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_SPLAT)
-
-	# Create texture set
-	# NOTE: usually this is not made from script, it can be built with editor tools
-	var texture_set = HTerrainTextureSet.new()
-	texture_set.set_mode(HTerrainTextureSet.MODE_TEXTURES)
-	texture_set.insert_slot(-1)
-	texture_set.set_texture(0, HTerrainTextureSet.TYPE_ALBEDO_BUMP, grass_texture)
-	# Create terrain node
-	var terrain = HTerrain.new()
-	terrain.set_shader_type(HTerrain.SHADER_CLASSIC4_LITE)
-	terrain.set_data(terrain_data)
-	terrain.set_texture_set(texture_set)
-	add_child(terrain, true)
-
-	# No need to call this, but you may need to if you edit the terrain later on
-	terrain.update_collider()
 
 func player_join(peer_id):
 	print("Joined player id: " + str(peer_id))
@@ -147,7 +159,7 @@ func player_join(peer_id):
 
 	if get_tree().get_multiplayer().is_server():
 		print("syncring map and timer")
-		self.receive_seeds.rpc_id(peer_id, self.noise_seed)
+		self.receive_seeds.rpc_id(peer_id, self.noise_seed, terrain_data_2)
 		Globals.synchronize_timer(Globals.timer)
 		print("finish :D")
 
