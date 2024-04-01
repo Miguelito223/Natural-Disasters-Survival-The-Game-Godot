b3a37fea124cf0be9c21e3ba2c09905fd0394595
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-04-01 12:54:25 +0200

    fixed

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -22,6 +22,7 @@ var noise_multiplier = 50.0
 
 # You may want to change paths to your own textures
 var grass_texture = preload("res://Textures/texture-grass-field.jpg")
+var rock_texture = preload("res://Textures/dark-cracked-concrete-wall.jpg")
 
 
 @onready var timer = $Timer
@@ -57,9 +58,96 @@ func _ready():
 		get_tree().get_multiplayer().peer_disconnected.connect(player_disconect)
 
 		if not OS.has_feature("dedicated_server") and get_tree().get_multiplayer().is_server():
+			generate_seed()
 			player_join(1)	
 		elif OS.has_feature("dedicated_server") and get_tree().get_multiplayer().is_server():
+			generate_seed()
 			Globals.synchronize_timer(Globals.timer)
+
+func generate_seed():
+	if not Globals.is_networking:
+		noise_seed = randi()
+		_recive_seed(noise_seed)
+	else:
+		if get_tree().get_multiplayer().is_server():
+			noise_seed = randi()	
+
+@rpc("any_peer", "call_local")
+func _recive_seed(seed):
+	#generate seed
+	noise_seed = seed
+	noise.seed = seed
+
+	#Await 5 seconds
+	await get_tree().create_timer(1).timeout
+
+	#generate world
+	generate_world()
+
+func generate_world():
+
+	# Create terrain resource and give it a size.
+	# It must be either 513, 1025, 2049 or 4097.
+	var terrain_data = HTerrainData.new()
+	terrain_data.resize(2049)
+
+	# Get access to terrain maps
+	var heightmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
+	var normalmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
+	var splatmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
+
+	# Generate terrain maps
+	# Note: this is an example with some arbitrary formulas,
+	# you may want to come up with your owns
+	for z in heightmap.get_height():
+		for x in heightmap.get_width():
+			# Generate height
+			var h = noise_multiplier * noise.get_noise_2d(x, z)
+
+			# Getting normal by generating extra heights directly from noise,
+			# so map borders won't have seams in case you stitch them
+			var h_right = noise_multiplier * noise.get_noise_2d(x + 0.1, z)
+			var h_forward = noise_multiplier * noise.get_noise_2d(x, z + 0.1)
+			var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
+
+			# Generate texture amounts
+			var splat = splatmap.get_pixel(x, z)
+			var slope = 4.0 * normal.dot(Vector3.UP) - 2.0
+			# Snow on the slopes
+			var snow_amount = clamp(1.0 - slope, 0.0, 1.0)
+			# Rock below sea level
+			var rock_amount = clamp(0.0 - h, 0.0, 1.0)
+			splat = splat.lerp(Color(0,1,0,0), rock_amount)
+			splat = splat.lerp(Color(0,0,1,0), snow_amount)
+
+			heightmap.set_pixel(x, z, Color(h, 0, 0))
+			normalmap.set_pixel(x, z, HTerrainData.encode_normal(normal))
+			splatmap.set_pixel(x, z, splat)
+
+	# Commit modifications so they get uploaded to the graphics card
+	var modified_region = Rect2(Vector2(), heightmap.get_size())
+	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_HEIGHT)
+	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_NORMAL)
+	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_SPLAT)
+
+	# Create texture set
+	# NOTE: usually this is not made from script, it can be built with editor tools
+	var texture_set = HTerrainTextureSet.new()
+	texture_set.set_mode(HTerrainTextureSet.MODE_TEXTURES)
+	texture_set.insert_slot(-1)
+	texture_set.set_texture(0, HTerrainTextureSet.TYPE_ALBEDO_BUMP, grass_texture)
+	texture_set.insert_slot(-1)
+	texture_set.set_texture(1, HTerrainTextureSet.TYPE_ALBEDO_BUMP, rock_texture)
+
+	# Create terrain node
+	var terrain = HTerrain.new()
+	terrain.set_shader_type(HTerrain.SHADER_CLASSIC4_LITE)
+	terrain.set_data(terrain_data)
+	terrain.set_texture_set(texture_set)
+	add_child(terrain)
+
+	# No need to call this, but you may need to if you edit the terrain later on
+	terrain.update_collider()
 
 	
 func player_join(peer_id):
@@ -73,12 +161,15 @@ func player_join(peer_id):
 	add_child(player, true)
 
 	if get_tree().get_multiplayer().is_server():
-		print("syncring timer")
-		Globals.synchronize_timer(Globals.timer)
-		print("finish :D")
+		if peer_id in Globals.players_conected_list:
+			print("syncring timer and map")
+			Globals.synchronize_timer(Globals.timer)
+			_recive_seed.rpc_id(peer_id, noise_seed)
+			print("finish :D")
 
 func player_disconect(peer_id):
 	print("Disconected player id: " + str(peer_id))
+	await get_tree().create_timer(5).timeout
 	var player = get_node(str(peer_id))
 	if is_instance_valid(player):
 		print("the instance is valid")
