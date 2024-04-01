f4c3fb676adc175364dfb147c7fde9d9a0bb7954
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-04-01 09:52:17 +0200

    fixed terrain

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -50,87 +50,17 @@ func _ready():
 	Globals.map = self
 
 	if not Globals.is_networking:
-		$Timer.wait_time = Globals.timer
-		$Timer.start()
-		generate_seed()
+		Globals.synchronize_timer(Globals.timer)
 	else:
 
 		get_tree().get_multiplayer().peer_connected.connect(player_join)
 		get_tree().get_multiplayer().peer_disconnected.connect(player_disconect)
 
 		if not OS.has_feature("dedicated_server") and get_tree().get_multiplayer().is_server():
-			generate_seed()	
 			player_join(1)	
 		elif OS.has_feature("dedicated_server") and get_tree().get_multiplayer().is_server():
-			generate_seed()
 			Globals.synchronize_timer(Globals.timer)
-		
-func generate_seed():
-	if not Globals.is_networking:
-		noise_seed = randi()
-		receive_seeds(noise_seed)
-	else:
-		if get_tree().get_multiplayer().is_server():
-			noise_seed = randi()
 
-@rpc("any_peer", "call_local")
-func receive_seeds(received_noise_seed):
-	print("Recived seeds :D")
-	noise_seed = received_noise_seed
-	noise.seed = noise_seed
-	generate_terrain()
-
-
-func generate_terrain():
-	var terrain_data = HTerrainData.new()
-	terrain_data.resize(4097)
-
-	var heightmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
-	var normalmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
-	var splatmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
-
-	# Iterar sobre cada punto del mundo
-	for z in range(heightmap.get_height()):
-		for x in range(heightmap.get_width()):
-			# Generar altura
-			var h = noise_multiplier * noise.get_noise_2d(x, z)
-
-			# Obtener la normal
-			var h_right = noise_multiplier * noise.get_noise_2d(x + 0.1, z)
-			var h_forward = noise_multiplier * noise.get_noise_2d(x, z + 0.1)
-			var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
-
-			# Generar cantidades de textura
-			var splat = splatmap.get_pixel(x, z)
-
-			# Almacenar los resultados en la imagen temporal
-			heightmap.set_pixel(x, z, Color(h, 0, 0))
-			normalmap.set_pixel(x, z, HTerrainData.encode_normal(normal))
-			splatmap.set_pixel(x, z, splat)
-
-	# Commit modifications so they get uploaded to the graphics card
-	var modified_region = Rect2(Vector2(), heightmap.get_size())
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_HEIGHT)
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_NORMAL)
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_SPLAT)
-
-	# Create texture set
-	# NOTE: usually this is not made from script, it can be built with editor tools
-	var texture_set = HTerrainTextureSet.new()
-	texture_set.set_mode(HTerrainTextureSet.MODE_TEXTURES)
-	texture_set.insert_slot(-1)
-	texture_set.set_texture(0, HTerrainTextureSet.TYPE_ALBEDO_BUMP, grass_texture)
-	# Create terrain node
-	var terrain = HTerrain.new()
-	terrain.set_shader_type(HTerrain.SHADER_CLASSIC4_LITE)
-	terrain.set_data(terrain_data)
-	terrain.set_texture_set(texture_set)
-	add_child(terrain, true)
-
-	# No need to call this, but you may need to if you edit the terrain later on
-	terrain.update_collider()
-
-	return terrain
 	
 func player_join(peer_id):
 	print("Joined player id: " + str(peer_id))
@@ -143,8 +73,7 @@ func player_join(peer_id):
 	add_child(player, true)
 
 	if get_tree().get_multiplayer().is_server():
-		print("syncring map and timer")
-		receive_seeds.rpc_id(peer_id, noise_seed)
+		print("syncring timer")
 		Globals.synchronize_timer(Globals.timer)
 		print("finish :D")
 
