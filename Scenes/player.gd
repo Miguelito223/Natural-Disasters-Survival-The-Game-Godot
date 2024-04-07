d230ada57cf10660f6170025db24bfcd57d76721
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-04-07 17:34:41 +0200

    fix

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -289,6 +289,9 @@ func _reset_player():
 	body_temperature = 37
 	body_oxygen = Max_oxygen
 	body_bradiation = min_bdradiation
+	velocity = Vector3(0,0,0)
+
+	
 	var rand_pos = Vector3(randi_range(0,2049),1000,randi_range(0,2049))
 	var space_state = self.get_world_3d().direct_space_state
 	var ray = PhysicsRayQueryParameters3D.create(rand_pos, rand_pos - Vector3(0,10000,0))
