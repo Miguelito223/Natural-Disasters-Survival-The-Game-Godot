c896123a81f513e050f208f284b1644e845a7459
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-04-07 17:44:22 +0200

    fixed meteor scale

--- Scenes/meteors.gd
+++ Scenes/meteors.gd
@@ -1,12 +1,13 @@
 extends RigidBody3D
 
 var explosion_scene = preload("res://Scenes/explosion.tscn")
-var body_scale = Vector3(randi_range(1,50),randi_range(1,50),randi_range(1,50))
+var rand_num = randi_range(1,50)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CollisionShape3D.scale = body_scale
-	$CSGSphere3D.scale = body_scale
-	$Fire.scale = body_scale
+	$CollisionShape3D.scale = Vector3(rand_num,rand_num,rand_num)
+	$CSGSphere3D.scale = Vector3(rand_num,rand_num,rand_num)
+	$Fire.scale = Vector3(rand_num,rand_num,rand_num)
 	self.mass = 3 / 4 * PI * $CSGSphere3D.radius
 	self.gravity_scale = Globals.gravity
 
@@ -16,7 +17,7 @@ func _on_body_entered(_body):
 
 	var explosion_node = explosion_scene.instantiate()
 	explosion_node.position = self.position
-	explosion_node.scale = body_scale
+	explosion_node.scale = Vector3(rand_num,rand_num,rand_num)
 	get_parent().add_child(explosion_node, true)
 	self.queue_free()
 
