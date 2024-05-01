b166e2f39388486b112289a079ffab0ac4bb2878
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-05-01 08:52:41 +0200

    fixed

--- Scripts/Chat.gd
+++ Scripts/Chat.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
 
-
 var msm
 
 @rpc("any_peer", "call_local")
 func msg_rpc(username, data):
 	$TextEdit.text +=  str(username, ": ", data, "\n")
+	$TextEdit.text = ""
+	$TextEdit.scroll_vertical = INF
 
 func _on_button_pressed():
 	msg_rpc.rpc(Globals.username, $LineEdit.text)
