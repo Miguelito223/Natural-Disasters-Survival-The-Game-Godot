e4ce895e30d0e7eeda40e17779e797127d864fd5
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-05-08 14:14:07 +0200

    fixed

--- Scripts/Warning Hud.gd
+++ Scripts/Warning Hud.gd
@@ -1,5 +1,9 @@
 extends CanvasLayer
 
+func _enter_tree():
+	if Globals.is_networking:
+		set_multiplayer_authority(multiplayer.get_unique_id())
+
 func _ready() -> void:
 	if Globals.is_networking:
 		if not is_multiplayer_authority():
@@ -10,7 +14,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if get_parent().get_parent().started:
-		$Label.text = "Current Disasters/Weather is: \n" + get_parent().get_parent().current_weather_and_disaster + "\nTime Left for the next disasters: \n" + str(int(get_parent().get_parent().get_node("Timer").time_left)) + "\nTime:\n" + str(Globals.Hour) + ":" + str(Globals.Minute)
+	if get_parent().started:
+		$Label.text = "Current Disasters/Weather is: \n" + get_parent().current_weather_and_disaster + "\nTime Left for the next disasters: \n" + str(int(get_parent().get_node("Timer").time_left)) + "\nTime:\n" + str(Globals.Hour) + ":" + str(Globals.Minute)
 	else:
-		$Label.text = "Waiting for players... Time remain: \n" + str(int(get_parent().get_parent().get_node("Timer").time_left))
+		$Label.text = "Waiting for players... Time remain: \n" + str(int(get_parent().get_node("Timer").time_left))
