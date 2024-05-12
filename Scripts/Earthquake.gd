15e223bfe37b653946b393e7ec56a7e2e1cf17dc
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-05-12 18:06:38 +0200

    fixed

--- Scripts/Earthquake.gd
+++ Scripts/Earthquake.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var magnitude = 0
+@export var magnitude: float = 7
 var magnitude_modifier = 0
 var next_physics_time = Time.get_ticks_msec()
 var SpawnTime = Time.get_ticks_msec()
@@ -12,10 +12,11 @@ var Life = [15,20]
 @onready var earthquake_sound = $earquake_sound
 @onready var earthqueake_aftershot_sound = $earqueake_aftershot
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	magnitude_modulate_sound()
-	magnitude_modifier_increment()
 	process_magnitude()
+	magnitude_modifier_increment(delta)
+	
 
 func _ready() -> void:
 	PlayInitialSounds()
@@ -38,223 +39,6 @@ func send_clientside_effects(ply, offset_ang, amplitude):
 	if randi_range(1, 8) == 1:
 		ply.camera_node._camera_shake(amplitude)
 
-func create_earthquake_with_parent():
-	var decider = randi() % int(floor(magnitude * 2)) == 1
-	if not decider:
-		if int(floor(magnitude)) > 1:
-			earthqueake_aftershot_sound.play()
-			var aftershock_magnitude = clamp(int(floor(magnitude)) - randi() % 3, 1, 10)
-			var aftershock = load("res://Scenes/earthquake.tscn").instantiate()
-			aftershock.magnitude = aftershock_magnitude
-			aftershock.position = Vector3.ZERO
-			get_parent().add_child(aftershock)
-			aftershock.global_transform.origin = get_parent().global_transform.origin
-			aftershock.show()
-			
-	else:
-		earthqueake_aftershot_sound.play()
-		var foreshock_magnitude = clamp(int(floor(magnitude)) - randi() % 3, 1, 10)
-		var foreshock = load("res://Scenes/earthquake.tscn").instantiate()
-		foreshock.magnitude = foreshock_magnitude
-		foreshock.position = position
-		get_parent().add_child(foreshock)
-		foreshock.global_transform.origin = get_parent().global_transform.origin
-		foreshock.show()
-
-func magnitude_modifier_increment():
-	# Ajustar el valor de MagnitudeModifier
-	self.magnitude_modifier = clamp(self.magnitude_modifier + (get_physics_process_delta_time() / 4), 0, 1)
-
-func get_real_magnitude():
-	return magnitude * magnitude_modifier
-
-func process_magnitude():
-	var magmod = magnitude_modifier
-	var mag = magnitude * magmod
-	
-	if 0 <= mag and mag < 1:
-		pass
-	elif 1 <= mag and mag < 2:
-		magnitude_one()
-	elif 2 <= mag and mag < 3:
-		magnitude_two()
-	elif 3 <= mag and mag < 4:
-		magnitude_three()
-	elif 4 <= mag and mag < 5:
-		magnitude_four()
-	elif 5 <= mag and mag < 6:
-		magnitude_five()
-	elif 6 <= mag and mag < 7:
-		magnitude_six()
-	elif 7 <= mag and mag < 8:
-		magnitude_seven()
-	elif 8 <= mag and mag < 9:
-		magnitude_eight()
-	elif 9 <= mag and mag < 10:
-		magnitude_nine()
-	elif 10 <= mag and mag < 11:
-		magnitude_ten()
-	elif 11 <= mag and mag < 12:
-		magnitude_eleven()
-	elif 12 <= mag and mag < 13:
-		magnitude_twelve()
-	else:
-		pass
-
-func magnitude_one():
-	var percentage = clamp(magnitude / 1.99, 0, 1)
-	var bxa = randi_range(-5, 5) / 100
-	var bya = randi_range(-5, 5) / 100
-	var mxa = (randi_range(-4, 4) / 100) * percentage
-	var mya = (randi_range(-4, 4) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-
-	do_physics()
-
-func magnitude_two():
-	var percentage = clamp(magnitude / 2.99, 0, 1)
-	var bxa = randi_range(-10, 10) / 100
-	var bya = randi_range(-10, 10) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-
-	do_physics()
-
-func magnitude_three():
-	var percentage = clamp(magnitude / 3.99, 0, 1)
-	var bxa = randi_range(-15, 15) / 100
-	var bya = randi_range(-15, 15) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-
-	
-	do_physics()
-
-func magnitude_four():
-	var percentage = clamp(magnitude / 4.99, 0, 1)
-	var bxa = randi_range(-20, 20) / 100
-	var bya = randi_range(-20, 20) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-
-	
-	do_physics()
-
-func magnitude_five():
-	var percentage = clamp(magnitude / 5.99, 0, 1)
-	var bxa = randi_range(-25, 25) / 100
-	var bya = randi_range(-25, 25) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-
-	
-	do_physics()
-
-func magnitude_six():
-	var percentage = clamp(magnitude / 6.99, 0, 1)
-	var bxa = randi_range(-30, 30) / 100
-	var bya = randi_range(-30, 30) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-	
-	do_physics()
-
-func magnitude_seven():
-	var percentage = clamp(magnitude / 7.99, 0, 1)
-	var bxa = randi_range(-35, 35) / 100
-	var bya = randi_range(-35, 35) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-	
-	do_physics()
-
-func magnitude_eight():
-	var percentage = clamp(magnitude / 8.99, 0, 1)
-	var bxa = randi_range(-40, 40) / 100
-	var bya = randi_range(-40, 40) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-	
-	do_physics()
-
-func magnitude_nine():
-	var percentage = clamp(magnitude / 9.99, 0, 1)
-	var bxa = randi_range(-45, 45) / 100
-	var bya = randi_range(-45, 45) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-	
-	do_physics()
-
-func magnitude_ten():
-	var percentage = clamp(magnitude / 10.99, 0, 1)
-	var bxa = randi_range(-50, 50) / 100
-	var bya = randi_range(-50, 50) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-	
-	do_physics()
-
-func magnitude_eleven():
-	var percentage = clamp(magnitude / 11.99, 0, 1)
-	var bxa = randi_range(-55, 55) / 100
-	var bya = randi_range(-55, 55) / 100
-	var mxa = (randi_range(-5, 5) / 100) * percentage
-	var mya = (randi_range(-5, 5) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-	
-	do_physics()
-
-func magnitude_twelve():
-	var percentage = clamp(magnitude / 12.99, 0, 1)
-	var bxa = randi_range(-1250, 1250) / 100
-	var bya = randi_range(-1250, 1250) / 100
-	var mxa = (randi_range(-425, 425) / 100) * percentage
-	var mya = (randi_range(-425, 425) / 100) * percentage
-	var xa = bxa + mxa
-	var ya = bya + mya
-
-	
-	do_physics()
-
-func unfreeze(v, mag):
-	if randi_range(1, 1024 - (25.6 * magnitude)) == 1:
-		v.freeze = false
-	if randi_range(1, 512 - (25.6 * magnitude)) == 1:
-		v.sleeping = false
-		v.freeze = false
-
 func can_do_physics(next_time):
 	if Engine.get_frames_per_second() > 0:  # Asegúrate de que no estemos dividiendo por cero
 		var current_time = Engine.get_frames_drawn() / Engine.get_frames_per_second()  # Obtener el tiempo actual del juego
@@ -265,6 +49,114 @@ func can_do_physics(next_time):
 				self.next_physics_time = current_time + next_time
 			return true
 	return false
+
+func do_physics():
+	var t = 0.1 # Obtener el valor del ConVar "gdisasters_envearthquake_simquality"
+	var mag = magnitude * magnitude_modifier
+
+	print("w2eñdèwod'0wd")
+	
+	# Si no podemos hacer física en este momento o la magnitud es menor que 3, no hacemos nada
+	if mag < 3:
+		print("Nuh uh")
+		return
+	
+	var vec = (mag * 25) * Vector3(randi_range(-15, 15) / 10, randi_range(-5, 4) / 10, randi_range(-5, 4) / 10)
+	var ang_vv = Vector3((randi_range(-15, 15) / 10), randi_range(-5, 4) / 10, randi_range(-5, 4) / 10) * (mag * 8)
+	
+	# Si hay una posibilidad de golpear, incrementamos la velocidad angular
+	if Globals.hit_chance(2):
+		ang_vv *= 20
+	
+	# Aplicar efectos a los jugadores
+	for v in get_tree().get_nodes_in_group("player"):
+		print(v)
+		if v.is_on_floor():
+			if 3 <= mag < 4:
+				pass
+			elif 4 <= mag < 5:
+				pass
+			elif 5 <= mag < 6:
+				pass
+			elif 6 <= mag < 7:
+				pass
+			elif 7 <= mag < 8:
+				v.set_velocity(vec)
+			elif 8 <= mag < 9:
+				v.set_velocity(vec * 1.125)
+			elif 9 <= mag < 10:
+				v.set_velocity(vec * 1.5)
+			elif 10 <= mag < 11:
+				v.set_velocity(vec * 2)
+			elif 11 <= mag < 12:
+				v.set_velocity(vec * 2.125)
+			elif 12 <= mag < 13:
+				v.set_velocity(vec * 2.5)
+	
+	# Aplicar efectos a las entidades
+	for v in get_tree().get_nodes_in_group("movable_objects"):
+		print(v)
+		if v.get_class() == "RigidBody3D":
+			var vel_mod = 1 - clamp(v.get_linear_velocity().length() / 2000, 0, 1)
+			var ang_v = ang_vv * vel_mod
+			
+			if 3 <= mag < 4:
+				if randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v)
+					v.add_central_impulse(ang_v)
+			elif 4 <= mag < 5:
+				if randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v)
+					v.add_central_impulse(ang_v)
+					unfreeze(v, mag)
+			elif 5 <= mag < 6:
+				if randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v)
+					v.add_central_impulse(ang_v)
+					unfreeze(v, mag)
+			elif 6 <= mag < 7:
+				if randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v * 2)
+					v.add_central_impulse(ang_v)
+					unfreeze(v, mag)
+			elif 7 <= mag < 8:
+				if  randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v * 4)
+					v.add_central_impulse(ang_v * 2)
+					unfreeze(v, mag)
+			elif 8 <= mag < 9:
+				if randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v * 8)
+					v.add_central_impulse(ang_v * 4)
+					unfreeze(v, mag)
+			elif 9 <= mag < 10:
+				if randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v * 12)
+					v.add_central_impulse(ang_v * 6)
+					unfreeze(v, mag)
+			elif 10 <= mag < 11:
+				if  randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v * 24)
+					v.add_central_impulse(ang_v * 12)
+					unfreeze(v, mag)
+			elif 11 <= mag < 12:
+				if randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v * 36)
+					v.add_central_impulse(ang_v * 24)
+					unfreeze(v, mag)
+			elif 12 <= mag < 13:
+				if randi_range(1, 2) == 1:
+					v.add_torque_impulse(ang_v * 40)
+					v.add_central_impulse(ang_v * 30)
+					unfreeze(v, mag)
+
+	
+func unfreeze(v, mag):
+	if randi_range(1, 1024 - (25.6 * magnitude)) == 1:
+		v.freeze = false
+	if randi_range(1, 512 - (25.6 * magnitude)) == 1:
+		v.sleeping = false
+		v.freeze = false
 
 func magnitude_modulate_sound():
 	var volume = self.magnitude  # Asumiendo que self.magnitude es una propiedad que representa la magnitud del terremoto
@@ -279,99 +171,196 @@ func magnitude_modulate_sound():
 		distance_mod = 1 - (ray_result["position"].distance_to(local_player_pos) / 3000)
 
 	vol_mod *= distance_mod
-    
+	
 	earthquake_sound.play()
 	earthquake_sound.volume_db = vol_mod
 
-func do_physics():
-	var t = 0.1 # Obtener el valor del ConVar "gdisasters_envearthquake_simquality"
-	var mag = magnitude * magnitude_modifier
-	
-	# Si no podemos hacer física en este momento o la magnitud es menor que 3, no hacemos nada
-	if !can_do_physics(t) or mag < 3:
-		return
-	
-	var vec = (mag * 25) * Vector3(randi_range(-15, 15) / 10, 0, randi_range(-5, 4) / 10)
-	var ang_vv = Vector3((randi_range(-15, 15) / 10), 0, randi_range(-5, 4) / 10) * (mag * 8)
-	
-	# Si hay una posibilidad de golpear, incrementamos la velocidad angular
-	if Globals.hit_chance(2):
-		ang_vv *= 20
-	
-	# Aplicar efectos a los jugadores
-	for v in get_tree().get_nodes_in_group("player"):
-		print(v)
-		if v.is_on_floor():
-			match mag:
-				3.0 < 4.0:
-					pass
-				4.0 < 5.0:
-					pass
-				5.0 < 6.0:
-					pass
-				6.0 < 7.0:
-					pass
-				7.0 < 8.0:
-					v.set_velocity(vec)
-				8.0 < 9.0:
-					v.set_velocity(vec * 1.125)
-				9.0 < 10.0:
-					v.set_velocity(vec * 1.5)
-				10.0 < 11.0:
-					v.set_velocity(vec * 2)
-				11.0 < 12.0:
-					v.set_velocity(vec * 2.125)
-				12.0 < 13.0:
-					v.set_velocity(vec * 2.5)
-	
-	# Aplicar efectos a las entidades
-	for v in get_tree().get_nodes_in_group("movable_objects"):
-		print(v)
-		if v.get_class() == "RigidBody3D":
-			var vel_mod = 1 - clamp(v.get_linear_velocity().length() / 2000, 0, 1)
-			var ang_v = ang_vv * vel_mod
+
+func create_earthquake_with_parent():
+	var decider = randi() % int(floor(magnitude * 2)) == 1
+	if not decider:
+		if int(floor(magnitude)) > 1:
+			earthqueake_aftershot_sound.play()
+			var aftershock_magnitude = clamp(int(floor(magnitude)) - randi() % 3, 1, 10)
+			var aftershock = load("res://Scenes/earthquake.tscn").instantiate()
+			aftershock.magnitude = aftershock_magnitude
+			aftershock.position = Vector3.ZERO
+			get_parent().add_child(aftershock, true)
+			aftershock.global_transform.origin = get_parent().global_transform.origin
+			aftershock.show()
 			
-			match mag:
-				3.0 < 4.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v)
-						v.add_central_impulse(ang_v)
-				4.0 < 5.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v)
-						v.add_central_impulse(ang_v)
-						unfreeze(v, mag)
-				5.0 < 6.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v)
-						v.add_central_impulse(ang_v)
-						unfreeze(v, mag)
-				6.0 < 7.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v * 2)
-						v.add_central_impulse(ang_v)
-						unfreeze(v, mag)
-				7.0 < 8.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v * 4)
-						v.add_central_impulse(ang_v * 2)
-						unfreeze(v, mag)
-				8.0 < 9.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v * 8)
-						v.add_central_impulse(ang_v * 4)
-						unfreeze(v, mag)
-				9.0 < 10.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v * 12)
-						v.add_central_impulse(ang_v * 6)
-						unfreeze(v, mag)
-				10.0 < 11.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v * 24)
-						v.add_central_impulse(ang_v * 12)
-						unfreeze(v, mag)
-				11.0 < 12.0:
-					if randi_range(1, 2) == 1:
-						v.add_torque_impulse(ang_v * 36)
+	else:
+		earthqueake_aftershot_sound.play()
+		var foreshock_magnitude = clamp(int(floor(magnitude)) - randi() % 3, 1, 10)
+		var foreshock = load("res://Scenes/earthquake.tscn").instantiate()
+		foreshock.magnitude = foreshock_magnitude
+		foreshock.position = position
+		get_parent().add_child(foreshock, true)
+		foreshock.global_transform.origin = get_parent().global_transform.origin
+		foreshock.show()
+
+func magnitude_modifier_increment(delta):
+	# Ajustar el valor de MagnitudeModifier
+	self.magnitude_modifier = clamp(self.magnitude_modifier + (delta / 4), 0, 1)
+
+func get_real_magnitude():
+	return magnitude * magnitude_modifier
+
+func process_magnitude():
+	var mag = magnitude * magnitude_modifier
+
+	print("Processing...", mag)
+	
+	if mag >= 0 and mag < 1:
+		print("nuh uh, very low")
+	elif mag >= 1 and mag < 2:
+		magnitude_one()
+	elif mag >= 2 and mag < 3:
+		magnitude_two()
+	elif mag >= 3 and mag < 4:
+		magnitude_three()
+	elif mag >= 4 and mag < 5:
+		magnitude_four()
+	elif mag >= 5 and mag < 6:
+		magnitude_five()
+	elif mag >= 6 and mag < 7:
+		magnitude_six()
+	elif mag >= 7 and mag < 8:
+		magnitude_seven()
+	elif mag >= 8 and mag < 9:
+		magnitude_eight()
+	elif mag >= 9 and mag < 10:
+		magnitude_nine()
+	elif mag >= 10 and mag < 11:
+		magnitude_ten()
+	elif mag >= 11 and mag < 12:
+		magnitude_eleven()
+	elif mag >= 12 and mag < 13:
+		magnitude_twelve()
+	else:
+		print("nuh uh")
+
+func magnitude_one():
+	var percentage = clamp(magnitude / 1.99, 0, 1)
+	var bxa = randi_range(-5, 5) / 100
+	var bya = randi_range(-5, 5) / 100
+	var mxa = (randi_range(-4, 4) / 100) * percentage
+	var mya = (randi_range(-4, 4) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_two():
+	var percentage = clamp(magnitude / 2.99, 0, 1)
+	var bxa = randi_range(-10, 10) / 100
+	var bya = randi_range(-10, 10) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_three():
+	var percentage = clamp(magnitude / 3.99, 0, 1)
+	var bxa = randi_range(-15, 15) / 100
+	var bya = randi_range(-15, 15) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_four():
+	var percentage = clamp(magnitude / 4.99, 0, 1)
+	var bxa = randi_range(-20, 20) / 100
+	var bya = randi_range(-20, 20) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_five():
+	var percentage = clamp(magnitude / 5.99, 0, 1)
+	var bxa = randi_range(-25, 25) / 100
+	var bya = randi_range(-25, 25) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya	
+	do_physics()
+
+func magnitude_six():
+	var percentage = clamp(magnitude / 6.99, 0, 1)
+	var bxa = randi_range(-30, 30) / 100
+	var bya = randi_range(-30, 30) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_seven():
+	var percentage = clamp(magnitude / 7.99, 0, 1)
+	var bxa = randi_range(-35, 35) / 100
+	var bya = randi_range(-35, 35) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_eight():
+	var percentage = clamp(magnitude / 8.99, 0, 1)
+	var bxa = randi_range(-40, 40) / 100
+	var bya = randi_range(-40, 40) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_nine():
+	var percentage = clamp(magnitude / 9.99, 0, 1)
+	var bxa = randi_range(-45, 45) / 100
+	var bya = randi_range(-45, 45) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_ten():
+	var percentage = clamp(magnitude / 10.99, 0, 1)
+	var bxa = randi_range(-50, 50) / 100
+	var bya = randi_range(-50, 50) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_eleven():
+	var percentage = clamp(magnitude / 11.99, 0, 1)
+	var bxa = randi_range(-55, 55) / 100
+	var bya = randi_range(-55, 55) / 100
+	var mxa = (randi_range(-5, 5) / 100) * percentage
+	var mya = (randi_range(-5, 5) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+func magnitude_twelve():
+	var percentage = clamp(magnitude / 12.99, 0, 1)
+	var bxa = randi_range(-1250, 1250) / 100
+	var bya = randi_range(-1250, 1250) / 100
+	var mxa = (randi_range(-425, 425) / 100) * percentage
+	var mya = (randi_range(-425, 425) / 100) * percentage
+	var xa = bxa + mxa
+	var ya = bya + mya
+	do_physics()
+
+
+
+
 
